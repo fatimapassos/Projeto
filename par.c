e2bff50d6ca566616a56799f24b655d51e2817cf
fatimapassos <fatima15passos@outlook.com>
2016-04-16 11:58:48 -0300

    mudança de codigo

--- par.c
+++ par.c
@@ -1,1 +1,12 @@
-fatima
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int numero;
+      printf("Digite um numero:");
+        scanf("%d",&umero);
+        if(numero % 2 == 0)
+        printf("O %d é par!", numero);
+        
+        return 0;
+}
